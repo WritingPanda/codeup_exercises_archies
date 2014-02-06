6df78b2f076cf32a83d6b27e10a61d8a2d6e4657
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-06 11:47:33 -0600

    Practicing with if statements and comparisons

---
+++ PHP/if.php
@@ -0,0 +1,40 @@
+<?php
+
+$a = 5;
+$b = 10;
+$c = '10';
+
+if ($a < $b) {
+    echo "$a is less than $b\n";
+}
+
+if ($b > $a) {
+    echo "$b is greater than $a\n";
+}
+
+if ($b >= $c) {
+    echo "$b is greater than or equal to $c\n";
+}
+
+if ($b <= $c) {
+    echo "$b is less than or equal to $c\n";
+}
+
+if ($b == $c) {
+    echo "$b is equal to $c\n";
+}
+
+if ($b === $c) {
+    echo "$b is identical to $c\n";
+}
+
+if ($b != $c) {
+    echo "$b is not equal to $c\n";
+}
+
+if ($b !== $c) {
+    echo "$b is not identical to $c\n";
+}
+
+// Omar is a happy panda with coding skills.
+?>
