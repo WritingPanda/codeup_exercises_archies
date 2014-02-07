1d900c7f4bfd4fdb85061068824cb8fb848d0b26
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-07 15:14:07 -0600

    Adding two features to check for values

--- PHP/RandomGame.php
+++ PHP/RandomGame.php
@@ -1,17 +1,27 @@
 <?php
+
 if ($argc == 3) {
-	//Get the starting number
-	$start = $argv[1];
-	//Get the ending number
-	$end = $argv[2];
+ 	//Get the starting number
+	$min = $argv[1];
+	 //Get the ending number
+	$max = $argv[2];
+} else {
+	echo "User error. Don't be a jerk.";
+	exit(0);
+}
+	//Checking if the number is numeral
+if (!is_numeric($min) || !is_numeric($max)) {
+	echo "Please use numbers.";
+	exit(0);
 }
 
-$number = mt_rand($start, $end);
-
+	//Getting the random number
+$number = mt_rand($min, $max);
+	//Introducing the game
 fwrite(STDOUT, 'Guess the number. ');
-
+	//The starting point
 $guess = 0;
-
+	//The user hasn't guessed anything
 $number_of_guesses = 0;
 
 while ($guess != $number) {
