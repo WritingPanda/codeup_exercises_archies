e0a85b663e680587109f6ffed1fdba821c004e63
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-07 20:05:40 -0600

    Messing aroundx2

--- PHP/for.php
+++ PHP/for.php
@@ -1,12 +1,26 @@
 <?php
 
-if ($argc == 3) {
-	$start = intval($argv[1]);
-	$end = intval($argv[2]);
+$start = $argv[1];
+$end = $argv[2];
+$increment = $argv[3];
+
+
+if ($start > $end) {
+	echo 'Please make sure that the second number is lower than the first one.' . PHP_EOL;
+	exit(0);
 }
 
-for (; $start <= $end; $start++) {
-	echo "$start\n";
+if (is_null($argv[3])) {
+	$increment = 1;
+}
+
+if (!is_numeric($start) || !is_numeric($end) || !is_numeric($increment)) {
+	echo 'Please use numbers.' . PHP_EOL;
+	exit(0);
+} 
+
+for ($i = $start; $i <= $end; $i+=$increment) {
+	echo "$i\n";
 }
 
 ?>
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-##This is a README file.
-
-#I like this README file. 
+##This is a README file. 
 
 I am learning how to code, and I am learning from the best programmers out there. I have been waiting to learn something like this for years, and now is my time. Hooray for me!
 
 I need to practice more with markdown because I really am not sure what are the commands for it. <-- note to self
 
 ##This is the end of this README file.
+
+###Hi Becky!
