6200ae7a4d664389bccdb7f4cfe0554147a0d136
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-10 14:40:12 -0600

    Adding comments and arrow.

--- PHP/break_continue.php
+++ PHP/break_continue.php
@@ -1,14 +1,16 @@
 <?php
 
+//Working with continue
 for ($i=1; $i <= 100; $i++) { 
 	echo $i . "\t";
 	if ($i % 2 == 0){
-		echo "Even.\n";
+		echo "<---Even.\n";
 		continue;
 	}
 	
 }
 
+//Working with break
 for ($i=1; $i <= 100 ; $i++) { 
 	echo $i . PHP_EOL;
 	if ($i == 10) {
