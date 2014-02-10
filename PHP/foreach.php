fc2b2c86ab623937c7da0cad7ea2893b0cd32774
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-09 20:34:15 -0600

    Checking which values are scalars.

--- PHP/foreach.php
+++ PHP/foreach.php
@@ -2,18 +2,10 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 foreach ($things as $value) {
-	if (is_int($value)) {
-		echo "\$value is an integer. ";
-	} elseif (is_double($value)) {
-		echo "\$value is a float. ";
-	} elseif (is_bool($value)) {
-		echo "\$value is a booleon. ";
-	} elseif (is_array($value)) {
-		echo "\$value is an array. ";
-	} elseif (is_null($value)) {
-		echo "\$value is null. ";
-	} elseif (is_string($value)) {
-		echo "\$value is a string. ";
+	if (is_scalar($value)) {
+		echo "{$value} is a scalar. ";
+	} else {
+		echo "{$value} is not a scalar. ";
 	}
 }
 
