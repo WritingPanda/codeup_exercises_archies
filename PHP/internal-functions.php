d666731d1bb3bc89643b93fcc6eed2de5cfd0e28
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-11 15:57:36 -0600

    Beginning internal functions exercise.

---
+++ PHP/internal-functions.php
@@ -0,0 +1,19 @@
+<?php
+
+$nothing = NULL;
+$something = '';
+$array = array(1,2,3);
+
+// Create a funciton that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
+
+// TEST: If var $nothing is set, display '$nothing is SET'
+
+// TEST: If var $nothing is empty, display '$nothing is EMPTY'
+
+// TEST: If var $something is set, display '$something is SET'
+
+// Serialize the array $array, and output the results
+
+// Unserialize the array $array, and output the results
+
+?>
