80265f0181fb8fa6a9832a54b4d847e711e53a3f
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-10 20:39:07 -0600

    Adding new lines for readability.

--- PHP/arithmetic.php
+++ PHP/arithmetic.php
@@ -5,23 +5,27 @@ function add($a, $b) {
 }
 
 add(50, 100);
+echo "\n";
 
 function subtract($a, $b) {
     echo $a - $b;
 }
 
-subtract(100 - 50);
+subtract(100, 50);
+echo "\n";
 
 function multiply($a, $b) {
     echo $a * $b;
 }
 
 multiply(2, 50);
+echo "\n";
 
 function divide($a, $b) {
     echo $a / $b;
 }
 
 divide(1000, 50);
+echo "\n";
 
 ?>
