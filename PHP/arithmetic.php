45a77c42e1830f8534ec3bd6d78a82ba9767ddde
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-10 20:32:07 -0600

    Completing functions and calling them.

---
+++ PHP/arithmetic.php
@@ -0,0 +1,27 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b;
+}
+
+add(50, 100);
+
+function subtract($a, $b) {
+    echo $a - $b;
+}
+
+subtract(100 - 50);
+
+function multiply($a, $b) {
+    echo $a * $b;
+}
+
+multiply(2, 50);
+
+function divide($a, $b) {
+    echo $a / $b;
+}
+
+divide(1000, 50);
+
+?>
