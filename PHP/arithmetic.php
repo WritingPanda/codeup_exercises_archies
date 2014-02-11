ad98781faf4bbca336cc2636243e08bc06ee0422
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-11 15:52:11 -0600

    Need to make more changes to make it right.

--- PHP/arithmetic.php
+++ PHP/arithmetic.php
@@ -4,64 +4,62 @@
 function error($a, $b) {
 	if (!is_numeric($a) || !is_numeric($b)) {
 		echo "\nERROR: Both arguments {$a} and {$b} must be numbers.\n\n";
-        return false;
-        exit(0);
-    } elseif (is_null($a) || is_null($b)) {
-        return false;
-        echo "ERROR: {$a} or {$b} has been left without a value.";
-        exit(0);
-    } else {
         return true;
+    } else {
+        return false;
     }
 }
 // Establishing add function
 function add($a, $b) {
-    error($a, $b);
-    return $a + $b;
+    if (!error($a, $b)) {
+        return $a + $b;
+    } 
 }
 // Calling it
 $sum = add(50, 25);
-	echo "The sum of 50 and 25 is {$sum}.\n";
+    echo $sum . PHP_EOL;;
+	
 
 // Establishing subtract function
 function subtract($a, $b) {
-    error($a, $b);
+    if (!error($a, $b)) 
     return $a - $b;
 }
 // Calling it
 $diff =	subtract(100, 50);
-	echo "The difference of 100 and 50 is {$diff}.\n";
+	echo $diff . PHP_EOL;;
 
 // Establishing multiply function
 function multiply($a, $b) {
-    error($a, $b);
+    if (!error($a, $b)){
 	return $a * $b;
-}
-// Calling it
-$product = multiply(4, 50);
-	echo "The product of 4 and 50 is {$product}.\n";
-
-// Establishing divide function
-function divide($a, $b) {
-    error($a, $b);
-    if ($b === 0) {
-        echo "ERROR: You cannot divide by 0.\t";
-        return false;
-        exit(0);
-    } else {
-        return $a / $b;
     }
 }
 // Calling it
+$product = multiply(4, 50);
+	echo $product . PHP_EOL;
+
+// Establishing divide function
+function divide($a, $b) {
+    if (!error($a, $b)){
+        return $a / $b;
+    } elseif ($b === 0) {
+        echo "ERROR: You cannot divide by 0.\t";
+        return false;
+    }
+}
+
+// Calling it
 $quotient = divide(1000, 50);
-	echo "The quotient of 1000 and 50 is {$quotient}.\n";
+	echo $quotient . PHP_EOL;
 
 // Establishing modulus function
 function modulus($a, $b) {
-    error($a, $b);
-    return $a % $b;
+    if (!error($a, $b)){
+        return $a % $b;
+    }   
 }
 // Calling it
 $module = modulus(336, 8);
-	echo "The remainder of 336 and 8 is {$module}.\n";
+	echo $module . PHP_EOL;;
 ?>
