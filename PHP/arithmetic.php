4a67200c9478b97ccc6fcb56f5dc5dec89a2f71a
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-11 09:35:19 -0600

    Continuing changing echoes to returns.

--- PHP/arithmetic.php
+++ PHP/arithmetic.php
@@ -5,32 +5,35 @@ function error($a, $b) {
 	if (!is_numeric($a) || !is_numeric($b)) {
 		echo "\nERROR: Both arguments {$a} and {$b} must be numbers.\n\n";
 	} elseif ($b === 0) {
-        echo "\nERROR: You cannot divide by 0.\n";
+        return false;
+    } elseif (is_null($a) || is_null($b)) {
+        return false;
+        echo "ERROR: {$a} or {$b} has been left without a value.";
     }
 }
 
 // Establishing add function
 function add($a, $b) {
     error($a, $b);
-    echo $a + $b;
+    return $a + $b;
 }
 // Calling it
-	add(50, 25);
-	echo "\n";
+$sum = add(50, 25);
+	echo "The sum of 50 and 25 is {$sum}.\n";
 
 // Establishing subtract function
 function subtract($a, $b) {
     error($a, $b);
-    echo $a - $b;
+    return $a - $b;
 }
 // Calling it
-	subtract(100, 50);
-	echo "\n";
+$diff =	subtract(100, 50);
+	echo "The difference of 100 and 50 is {$diff}.\n";
 
 // Establishing multiply function
 function multiply($a, $b) {
     error($a, $b);
-	echo $a * $b;
+	return $a * $b;
 }
 // Calling it
 	multiply(4, 50);
@@ -39,17 +42,17 @@ function multiply($a, $b) {
 // Establishing divide function
 function divide($a, $b) {
     error($a, $b);
-    echo $a / $b;
+    return $a / $b;
 }
 
 // Calling it
-	divide(1000,20);
+	divide(1000, 50);
 	echo "\n";
 
 // Establishing modulus function
 function modulus($a, $b) {
     error($a, $b);
-    echo $a % $b;
+    return $a % $b;
 }
 // Calling it
 	modulus(336, 8);
