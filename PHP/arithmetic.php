ea93d38940a0c99a64b63ae88ebf2041cc1bcf5e
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-10 21:21:44 -0600

    Trying to get it to work properly, as according to the exercise.

--- PHP/arithmetic.php
+++ PHP/arithmetic.php
@@ -1,70 +1,72 @@
 <?php
 
+// Establishing error function
 function error() {
-	if ($b == 0) {
-		echo "ERROR: You cannot divide by 0.\n";
-	} else {
-		echo "ERROR: Both arguments ($a and $b) must be numbers.\n";
+	if (!is_numeric($a) || !is_numeric($b)) {
+		echo "ERROR: Both arguments {$a} and {$b} must be numbers.\n";
 	}
 }
 
+// Establishing add function
 function add($a, $b) {
     if (is_numeric($a) && is_numeric($b)) {
     	echo $a + $b;
     } else {
-    	echo "ERROR: Both arguments ($a and $b) must be numbers.\n";
+    	error();
     }
 }
+// Calling it
+	add(50, 25);
+	echo "\n";
 
-add(50, 100);
-echo "\n";
-
+// Establishing subtract function
 function subtract($a, $b) {
     if (is_numeric($a) && is_numeric($b)) {
     	echo $a - $b;
     } else {
-    	echo "ERROR: Both arguments ($a and $b) must be numbers.\n";
+    	error();
     }
 }
+// Calling it
+	subtract(100, 50);
+	echo "\n";
 
-subtract(100, 50);
-echo "\n";
-
+// Establishing multiply function
 function multiply($a, $b) {
     if (is_numeric($a) && is_numeric($b)) {
     	echo $a * $b;
     } else {
-    	echo "ERROR: Both arguments ($a and $b) must be numbers.\n";
+    	error();
     }
 }
+// Calling it
+	multiply(2, 50);
+	echo "\n";
 
-multiply(2, 50);
-echo "\n";
-
+// Establishing divide function
 function divide($a, $b) {
     if (is_numeric($a) && is_numeric($b)) {
     	echo $a / $b;
-    } elseif ($b == 0) {
+    } elseif ($b === 0) {
     	echo "ERROR: You cannot divide by 0.";
     } else {
-    	echo "ERROR: Both arguments ($a and $b) must be numbers.\n";
-    }
+    	error();
+    } 
 }
+// Calling it
+	divide(1000,100);
+	echo "\n";
 
-divide(1000, 50);
-echo "\n";
-
-// Modulus function
-
+// Establishing modulus function
 function modulus($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
     	echo $a % $b;
     } else {
-    	echo "ERROR: Both arguments ($a and $b) must be numbers.\n";
+    	error();
     }
 }
-
-modulus(336, 8);
-echo "\n";
+// Calling it
+	modulus(336, 8);
+	echo "\n";
 
 ?>
