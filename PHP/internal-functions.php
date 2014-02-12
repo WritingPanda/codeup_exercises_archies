acbe1ef3a2c0bfc2a3a2ad2438d1d99ec2aee2ce
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-11 18:59:35 -0600

    Adding initial function.

--- PHP/internal-functions.php
+++ PHP/internal-functions.php
@@ -1,12 +1,24 @@
 <?php
 
+// Ignore step 3
+
 $nothing = NULL;
 $something = '';
 $array = array(1,2,3);
 
 // Create a funciton that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
 
+function setEmpty($checkvar) {
+	if (isset($checkvar)) {
+		echo "\$variable_name is SET.";
+	} elseif (empty($checkvar)) {
+		echo "\$variable_name is EMPTY.";
+	}
+}
+
 // TEST: If var $nothing is set, display '$nothing is SET'
+
+isEmpty($nothing);
 
 // TEST: If var $nothing is empty, display '$nothing is EMPTY'
 
