85a8721ed472abbd822c478222a101738ee33e8c
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-12 13:33:29 -0600

    THANK YOU JASON!!!

--- codeup_todo_list/todo.php
+++ codeup_todo_list/todo.php
@@ -3,33 +3,57 @@
 // Create array to hold list of todo items
 $items = array();
 
+// List array items formatted for CLI
+    // Return string of list items separated by newlines.
+    // Should be listed [KEY] Value like this:
+    // [1] TODO item 1
+    // [2] TODO item 2 - blah
+    // DO NOT USE ECHO, USE RETURN
+
+function list_items($list) {
+    $place = '';
+    // Iterate through list items
+    foreach ($list as $key => $item) {
+        $place .= "[" . ++$key . "]" . " " . $item . PHP_EOL;
+    // Display each item and a newline
+    }
+    return $place;
+}
+
+// Get STDIN, strip whitespace and newlines, 
+// and convert to uppercase if $upper is true
+function get_input($upper = FALSE) {
+    // Return filtered STDIN input
+    if($upper == TRUE) {
+        return strtoupper(trim(fgets(STDIN)));
+    } else {
+        return trim(fgets(STDIN));
+    }
+}
+
 // The loop!
 do {
-    // Iterate through list items
-    foreach ($items as $key => $item) {
-    		$key++;
-        // Display each item and a newline
-        echo "[{$key}] {$item}\n";
-    }
+    // Echo the list produced by the function
+    echo list_items($items);
 
     // Show the menu options
     echo '(N)ew item, (R)emove item, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
-    $input = trim(fgets(STDIN));
+    $input = get_input(true);
 
     // Check for actionable input
     if ($input == 'N' || $input == 'n') {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
-        $items[] = trim(fgets(STDIN));
+        $items[] = get_input();
     } elseif ($input == 'R' || $input == 'r') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
-        $key = trim(fgets(STDIN));
+        $key = get_input();
         // Remove from array
         unset($items[--$key]);
     }
