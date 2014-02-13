15a1c76db4430276a18e1f551503b2de976011d3
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-13 14:45:51 -0600

    Finishing the first part of the exercise.

--- PHP/listwithcommas.php
+++ PHP/listwithcommas.php
@@ -1,8 +1,11 @@
 <?php
 
 // Converts array into list n1, n2, ..., and n3
-function humanized_list($array) {
-	
+function humanized_list($input) { 
+	$array = explode(', ', $input);
+	$lastItem = array_pop($array);
+	$newArray = implode(", ", $array) . ", and " . $lastItem;
+	return $newArray;
 }
 
 // List of famous peeps
@@ -12,5 +15,5 @@ $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mal
 $famous_fake_physicists = humanized_list($physicists_string);
 
 // Output sentence
-echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
+echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.\n";
 ?>
