1792cbd1702adfe44a2351650530bd86709df838
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-13 10:52:06 -0600

    Completing exercise. Does not work, need to figure out why.

--- PHP/search_arrays.php
+++ PHP/search_arrays.php
@@ -3,12 +3,14 @@
 // first names
 $names = ['Tina', 'Dana', 'Mike', "Amy", 'Adam'];
 
-$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael']
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 // array_search(needle, haystack)
+$question = array_search('Tina', $names);
+var_dump($question);
 
-function check_array($array, $element) {
-	$query = array_search($array, $element);
+function check_array($element, $array) {
+	$query = array_search($element, $array);
 	if ($query !== FALSE) {
 		return TRUE;
 	} else {
@@ -16,5 +18,20 @@ function check_array($array, $element) {
 	}
 }
 
+function count_matches($needles, $haystack) {
+	$numberMatches = 0;
+	foreach ($needles as $needle) {
+		$result = array_search($needle, $haystack);
+		if (is_numeric($result)) {
+			$numberMatches++;
+		}
+	}
+}
+
+echo check_array('Tina', $names) . PHP_EOL;
+
+echo check_array('Bob', $names) . PHP_EOL;
+
+echo count_matches($compare, $names);
 
 ?>
