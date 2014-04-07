9973cd1888334171ccea15af758faf76a61f34de
WritingPanda <omar.quimbaya4@gmail.com>
2014-04-07 13:56:41 -0500

    Refactored for clean reading

--- Exercises/PHP/fizzbuzz.php
+++ Exercises/PHP/fizzbuzz.php
@@ -1,14 +1,15 @@
 <?php
 
-for ($i=1; $i <= 100 ; $i++) { 
+for ($i=1; $i <= 100; $i++) { 
 	if ($i % 3 == 0 && $i % 5 == 0) {
-		echo "FizzBuzz\n"; 
+		echo "FizzBuzz" . PHP_EOL; 
 	} elseif ($i % 3 == 0) {
-		echo "Fizz\n";
+		echo "Fizz" . PHP_EOL;
 	} elseif ($i % 5 == 0) {
-		echo "Buzz\n";
+		echo "Buzz" . PHP_EOL;
 	} else {
 		echo $i . PHP_EOL;
 	}
 }
+
 ?>
